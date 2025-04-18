64bec172f6ee7b2b253c01dac016a495dd496adb
adeyahya <adeyahyaprasetyo@gmail.com>
2025-04-19 04:20:40 +0700

    setup alias

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -2,8 +2,12 @@ set fish_greeting
 starship init fish | source
 pokemon-colorscripts -r
 
+# paths
+fish_add_path /home/adeyahya/.volta/bin
+
 # aliases
 alias z zellij
+alias neofetch fastfetch
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
