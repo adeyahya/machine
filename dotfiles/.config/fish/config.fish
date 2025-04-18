9efd1b9a632618fc267b4d79c52c9f62206197d2
adeyahya <adeyahyaprasetyo@gmail.com>
2025-04-19 03:18:25 +0700

    tmux and zellij

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,6 +1,10 @@
 set fish_greeting
 starship init fish | source
 pokemon-colorscripts -r
+
+# aliases
+alias z zellij
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
