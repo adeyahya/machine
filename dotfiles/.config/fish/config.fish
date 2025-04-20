0782414afc65b7474d85323bbf04894a403dc6e3
adeyahya <adeyahyaprasetyo@gmail.com>
2025-04-20 22:05:39 +0700

    config some vars

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,14 +1,17 @@
-set fish_greeting
-starship init fish | source
-pokemon-colorscripts -r
-
 # paths
 fish_add_path /home/adeyahya/.volta/bin
 
 # aliases
 alias z zellij
 alias neofetch fastfetch
+alias hx helix
+
+# vars
+set -gx EDITOR helix
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set fish_greeting
+    starship init fish | source
+    pokemon-colorscripts -r
 end
