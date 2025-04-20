5de464945c9cd947a7c1008fda7fe348fe2dd383
adeyahya <adeyahyaprasetyo@gmail.com>
2025-04-21 01:49:51 +0700

    update

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -9,9 +9,14 @@ alias hx helix
 # vars
 set -gx EDITOR helix
 
+# hooks
+eval (rbenv init - | source)
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
     starship init fish | source
-    pokemon-colorscripts -r
+    pokemon-colorscripts -r --no-title
 end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
